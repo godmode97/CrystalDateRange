728dd9c2f5dacf3cda61a332d172690257934736
neiljames <neil.monzales@shoresuite.com>
2018-08-29 14:55:06 +0800

    Initial Commit

---
+++ spec/date_range_spec.cr
@@ -0,0 +1,9 @@
+require "./spec_helper"
+
+describe DateRange do
+  # TODO: Write tests
+
+  it "works" do
+    false.should eq(true)
+  end
+end
---
+++ src/date_range.cr
@@ -0,0 +1,6 @@
+# TODO: Write documentation for `DateRange`
+module DateRange
+  VERSION = "0.1.0"
+
+  # TODO: Put your code here
+end
